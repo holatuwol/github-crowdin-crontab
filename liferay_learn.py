f89cfda610a7deb07a33ef55b3ad187f979d4fa7
Minhchau <holatuwol@gmail.com>
2021-05-26 22:14:00 -0700

    Ignore whether it is present in English when adding disclaimer (use target language)

--- liferay_learn.py
+++ liferay_learn.py
@@ -10,21 +10,19 @@ def add_disclaimers_to_learn(repository, language):
     os.chdir(repository.github.git_root)
 
     lstree_output = git.ls_tree('-r', '--name-only', repository.github.branch, strip=False)
-    all_files = get_eligible_files(repository, lstree_output, 'en')
+    all_files = get_eligible_files(repository, lstree_output, language)
 
     for file in all_files:
         if file[file.rfind('.')+1:] == 'html':
             continue
 
-        target_file = language + '/' + file[3:] if file[0:3] == 'en/' else file.replace('/en/', '/%s/' % language)
-
-        if not os.path.exists(target_file):
+        if not os.path.exists(file):
             continue
 
-        new_title, old_content, new_content = add_disclaimer_learn(target_file, language)
+        new_title, old_content, new_content = add_disclaimer_learn(file, language)
 
         if old_content != new_content:
-            with open(target_file, 'w') as f:
+            with open(file, 'w') as f:
                 f.write(new_title)
                 f.write('\n')
                 f.write(new_content)
