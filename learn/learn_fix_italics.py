216e70e0ec4cd706ae4fb2c3b4ec6139304afed8
Minhchau <holatuwol@gmail.com>
2022-01-10 21:39:36 -0800

    Ignore [%variable_name%] pattern Fix all the spacing around italics for all files when script runs

--- learn/learn_fix_italics.py
+++ learn/learn_fix_italics.py
@@ -28,10 +28,13 @@ def remove_tags(text, tag):
 def is_valid_next_to_italic(ch):
 	return ch.isspace() or ch in valid_punctuation
 
-def is_link_text(text, left, right, pos1):
-	if text.rfind(left, 0, pos1) <= text.rfind(right, 0, pos1):
+def is_link_text(text, left, right, start):
+	pos1 = text.rfind(left, 0, start)
+
+	if pos1 <= text.rfind(right, 0, start):
 		return False
 
+	pos1 = pos1 + 1
 	pos2 = text.find(right, pos1)
 
 	if pos2 == -1:
@@ -39,7 +42,10 @@ def is_link_text(text, left, right, pos1):
 
 	link_text = text[pos1:pos2]
 
-	return len(link_text) == len(urllib.parse.quote(link_text, safe=':/'))
+	if link_text[0] == '%' and link_text[-1] == '%':
+		return True
+
+	return len(link_text) == len(urllib.parse.quote(link_text, safe=':/?=#'))
 
 def fix_line_italics(line):
 	pos1 = line.find('_')
@@ -66,7 +72,7 @@ def fix_line_italics(line):
 			pos1 = pos1 - 1
 			pos2 = pos2 - 1
 
-		while len(line) > pos1+1 and line[pos1+1].isspace():
+		while pos1+2 < len(line) and line[pos1+1].isspace():
 			line = line[:pos1+1] + line[pos1+2:]
 			pos2 = pos2 - 1
 
@@ -75,6 +81,10 @@ def fix_line_italics(line):
 			pos1 = pos1 + 1
 			pos2 = pos2 + 1
 
+		if pos1+1 < len(line) and line[pos1+1] in valid_punctuation:
+			line = line[:pos1] + line[pos1+1] + '_' + line[pos1+2:]
+			pos1 = pos1 + 1
+
 		# strip all the whitespace before/after the italic marker, then re-add
 		# whatever whitespace is actually needed
 
@@ -82,47 +92,39 @@ def fix_line_italics(line):
 			line = line[:pos2-1] + line[pos2:]
 			pos2 = pos2 - 1
 
-		while len(line) > pos2+1 and line[pos2+1].isspace():
+		while pos2+2 < len(line) and line[pos2+1].isspace():
 			line = line[:pos2+1] + line[pos2+2:]
 
-		if len(line) > pos2+1 and not is_valid_next_to_italic(line[pos2+1]):
+		if pos2+2 < len(line) and not is_valid_next_to_italic(line[pos2+1]):
 			line = line[:pos2+1] + ' ' + line[pos2+1:]
-			pos2 = pos2 + 1
+
+		if pos2 > 0 and line[pos2-1] in valid_punctuation:
+			line = line[:pos2-1] + '_' + line[pos2-1] + line[pos2+1:]
+			pos2 = pos2 - 1
 
 		pos1 = line.find('_', pos2 + 1)
 
 	return line
 
-def fix_italics(input_file):
-	with open(input_file, 'r', encoding = 'utf-8') as f:
-		input_lines = f.readlines()
-
-	input_text = ''.join(input_lines)
-
+def extract_text(input_text):
 	html = _pandoc(input_text, '--from', 'markdown', '--to', 'html')
 
 	html = remove_tags(html, 'pre')
 	html = remove_tags(html, 'code')
 
-	text = _pandoc(html, '--from', 'html', '--to', 'plain', '--wrap=none')
+	return _pandoc(html, '--from', 'html', '--to', 'plain', '--wrap=none')
 
-	malformed_lines = [
-		line for line in text.split('\n')
-			if line.find('..') != 0 and line.find('_') != -1
-	]
-
-	if len(malformed_lines) == 0:
-		return
-
-	print(input_file)
-	print('\n'.join(malformed_lines))
+def fix_italics(input_file):
+	with open(input_file, 'r', encoding = 'utf-8') as f:
+		input_lines = f.readlines()
 
 	fixed_lines = []
+	malformed_lines = []
 
 	in_code_block = False
 	in_directive = False
 
-	for line in input_text.split('\n'):
+	for line in input_lines:
 		if line.find('```') == 0:
 			in_code_block = not in_code_block
 			fixed_lines.append(line)
@@ -148,15 +150,21 @@ def fix_italics(input_file):
 
 			in_directive = False
 
-		fixed_lines.append(fix_line_italics(line))
+		fixed_line = fix_line_italics(line)
 
-	output_text = '\n'.join(fixed_lines)
+		if fixed_line != line:
+			malformed_lines.append(line)
 
-	if input_text == output_text:
+		fixed_lines.append(fixed_line)
+
+	if len(malformed_lines) == 0:
 		return
 
+#	print(input_file)
+#	print(''.join(malformed_lines))
+
 	with open(input_file, 'w', encoding = 'utf-8') as f:
-		f.write(output_text)
+		f.write(''.join(fixed_lines))
 
 for file in sys.argv[1:]:
 	fix_italics(file)
